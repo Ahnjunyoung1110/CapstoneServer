46e37f1750d64067a9cc1e0c4711225db43323b1
Ahnjunyoung1110 <69619611+Ahnjunyoung1110@users.noreply.github.com>
2024-08-15 21:09:56 +0900

    Add files via upload

---
+++ Capstone_Server/src/test/java/com/Capstone/Capstone_Server/CapstoneServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Capstone.Capstone_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
